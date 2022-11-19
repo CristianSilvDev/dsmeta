24a39ec5a0e838738b8b76ea256be69160f8ee3a
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-19 19:11:53 -0300

    Send notification

--- frontend/src/components/SalesCard/idex.tsx
+++ frontend/src/components/SalesCard/idex.tsx
@@ -22,7 +22,7 @@ function SalesCard() {
         const dmin = minDate.toISOString().slice(0, 10);
         const dmax = maxDate.toISOString().slice(0, 10);
 
-        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+        axios.get(`${BASE_URL}/sales`)
             .then(response => {
                 setSales(response.data.content);
             });
@@ -75,7 +75,7 @@ function SalesCard() {
                                     <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={sale.id}/>
                                         </div>
                                     </td>
                                 </tr>
